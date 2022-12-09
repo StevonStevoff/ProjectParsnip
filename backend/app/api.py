987357a206d5e2eeb2dac8e2840c5226dd97ba69
Steven Taylor <Steven@taylorcrew.us>
2022-12-09 04:12:59 +0000

    updated precommit, and added base files for backend

---
+++ backend/app/api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["http://localhost:3000", "localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
