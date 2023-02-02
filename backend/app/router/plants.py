79dc38f5503f086f3261cb37bfbd1b3e22f5779e
Stuart <sc19sjjd@leeds.ac.uk>
2023-02-02 17:31:38 +0000

    Added Plant pydantic model

---
+++ backend/app/router/plants.py
@@ -0,0 +1,133 @@
+from fastapi import APIRouter, Depends, status
+
+# from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import get_async_session
+from app.models import Plant, User
+from app.router.utils import get_object_or_404
+from app.schemas import PlantCreate, PlantRead, PlantUpdate
+from app.users import current_active_superuser, current_active_user
+
+router = APIRouter()
+
+
+async def get_plant_or_404(
+    id: int, session: AsyncSession = Depends(get_async_session)
+) -> Plant:
+    return await get_object_or_404(id, Plant, session)
+
+
+@router.get(
+    "/",
+    name="plants:all_plants",
+    response_model=list[PlantRead],
+    dependencies=[Depends(current_active_superuser)],
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Missing token or inactive user.",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "No plants found",
+        },
+    },
+)
+async def get_all_plants(
+    session: AsyncSession = Depends(get_async_session), contains: str | None = None
+):
+    pass
+
+
+@router.get(
+    "/{id}",
+    response_model=PlantRead,
+    name="plants:plant",
+    dependencies=[Depends(current_active_superuser)],
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Missing token or inactive user.",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The plant does not exist.",
+        },
+    },
+)
+async def get_plant(
+    plant: Plant = Depends(get_plant_or_404),
+):
+    return PlantRead.from_orm(plant)
+
+
+@router.post(
+    "/register",
+    response_model=PlantCreate,
+    dependencies=[Depends(current_active_user)],
+    name="plants:register_plant",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Missing token or inactive user.",
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": "Not superuser or associated owner.",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The plant does not exist.",
+        },
+    },
+)
+async def register_plant(
+    plant_create: PlantCreate,
+    user: User = Depends(current_active_user),
+    session: AsyncSession = Depends(get_async_session),
+):
+    pass
+
+
+@router.delete(
+    "/{id}",
+    name="plants:plant",
+    dependencies=[Depends(current_active_user)],
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Missing token or inactive user.",
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": "Not superuser or associated device owner.",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The plant does not exist.",
+        },
+    },
+)
+async def delete_plant(
+    plant: Plant = Depends(get_plant_or_404),
+    user: User = Depends(current_active_user),
+    session: AsyncSession = Depends(get_async_session),
+):
+    pass
+
+
+@router.patch(
+    "/{id}",
+    response_model=PlantUpdate,
+    name="plants:patch_plant",
+    dependencies=[Depends(current_active_user)],
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Missing token or inactive user.",
+        },
+        status.HTTP_403_FORBIDDEN: {
+            "description": "Not superuser or associated device owner.",
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": "The plant does not exist.",
+        },
+    },
+)
+async def patch_plant(
+    plant_update=PlantUpdate,
+    plant: Plant = Depends(get_plant_or_404),
+    session: AsyncSession = Depends(get_async_session),
+):
+    pass
