1cee6ee14222ba52f6f72dda310a9da1a641a1a0
Steven Taylor <Steven@taylorcrew.us>
2023-02-05 18:48:09 +0000

    Setup branch for plant type tests

--- backend/app/tests/test_plant_types.py
+++ backend/app/tests/test_plant_types.py
@@ -28,7 +28,7 @@ async def add_plant_types(client):
 
 
 @pytest.mark.asyncio(scope="session")
-async def test_get_all_plant_profiles_without_token(client):
+async def test_get_all_plant_types_without_token(client):
     response = await client.get("/plant_types/")
     assert response.status_code == 401
     json_response = response.json()
