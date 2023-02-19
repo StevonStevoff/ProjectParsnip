18881180ed513abba1a7ace9b796913cd3c45714
Aimee <aimeeboyle6@gmail.com>
2023-02-19 01:13:09 +0000

    Basic profile user structure

--- frontend/src/screens/AuthScreens/ProfileScreen.jsx
+++ frontend/src/screens/AuthScreens/ProfileScreen.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
-import { useTheme } from '@react-navigation/native';
+import {
+  VStack, FormControl, Input, Button, useColorModeValue,
+} from 'native-base';
 import ProfilePicture from '../../components/ProfilePicture';
+import AuthUtils from '../../api/utils/AuthUtils';
 
 function ProfileScreen({ navigation }) {
+  const colorText = useColorModeValue('#3b3b3b', '#a9a9a9');
   return (
-    <View>
-      <ProfilePicture name="Jane Doe" username="@JDoe" />
+    <View style={{
+      width: '100%', height: '100%', flex: 1, justifyContent: 'center', alignItems: 'center',
+    }}
+    >
+      <ProfilePicture name="John Doe" username="Doed" />
+      <VStack space={3} alignItems="center" width="90%">
+        <FormControl>
+          <FormControl.Label color="red">
+            Name
+
+          </FormControl.Label>
+          <Input
+            variant="underlined"
+            value="John Doe"
+            _disabled={{
+              color: useColorModeValue('#000000', '#fafafa'),
+            }}
+          />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>Email Address</FormControl.Label>
+          <Input variant="underlined" value="johnDoe@gmail.com" />
+        </FormControl>
+        <VStack marginTop="24%" alignItems="center" width="90%">
+          <Button onPress={() => AuthUtils.logout()} w="40%" bg="error.600"> Log Out </Button>
+        </VStack>
+      </VStack>
     </View>
   );
 }
