ded7b55245a77317018577d1e291ca1943979997
Steven Taylor <Steven@taylorcrew.us>
2023-03-30 14:08:50 +0100

    Moved env read location

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,10 +1,9 @@
 import os
 from os.path import dirname, join
-from pathlib import Path
 
 from dotenv import load_dotenv
 
-dotenv_path = join(Path(dirname(__file__), "../../.env"))
+dotenv_path = join(dirname(__file__), "../.env")
 load_dotenv(dotenv_path)
 
 BACKEND_SECRET_KEY = os.environ.get("BACKEND_SECRET_KEY")
