55f7babffdf6c64989c00815fd70c2ae63ecff9a
Steven Taylor <Steven@taylorcrew.us>
2023-04-07 00:41:14 +0100

    Move location of .env file in backend so we can use a shared .env for both frontend and backend

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -3,7 +3,7 @@ from os.path import dirname, join
 
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), "../.env")
+dotenv_path = join(dirname(__file__), "../../.env")
 load_dotenv(dotenv_path)
 
 BACKEND_SECRET_KEY = os.environ.get("BACKEND_SECRET_KEY")
