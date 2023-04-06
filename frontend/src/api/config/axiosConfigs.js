3353d4c1a67cc3cd4d8ed4f5725ebc8d09b25c65
Steven Taylor <Steven@taylorcrew.us>
2023-04-07 00:29:17 +0100

    Config will now try to grab Urls from environment variables

--- frontend/src/api/config/axiosConfigs.js
+++ frontend/src/api/config/axiosConfigs.js
@@ -1,21 +1,26 @@
-import axios from 'axios';
-import { notification } from 'antd';
-import { Platform } from 'react-native';
+import axios from "axios";
+import { notification } from "antd";
+import { Platform } from "react-native";
 
-const androidDevUrl = 'http://10.43.204.119:8000';
+var backendUrl = process.env.BACKEND_URL;
+var androidDevUrl = process.env.ANDROID_DEV_URL;
+
+backendUrl = backendUrl === undefined ? "http://localhost:8000" : backendUrl;
+androidDevUrl =
+  androidDevUrl === undefined ? "http://10.43.204.119:8000" : androidDevUrl;
 
 const api = axios.create({
-  baseURL: Platform.OS === 'android' ? androidDevUrl : 'http://localhost:8000',
+  baseURL: Platform.OS === "android" ? androidDevUrl : backendUrl,
 });
 
 // defining a custom error handler for all APIs
 const errorHandler = (error) => {
   const statusCode = error.response?.status;
 
-  if (error.code === 'ERR_CANCELED') {
+  if (error.code === "ERR_CANCELED") {
     notification.error({
-      placement: 'bottomRight',
-      description: 'API canceled!',
+      placement: "bottomRight",
+      description: "API canceled!",
     });
     return Promise.resolve();
   }
