7c7c88ef6074ec14d8db85ae8a0654f7ddd9768b
Stuart <sc19sjjd@leeds.ac.uk>
2023-04-11 18:34:27 +0100

    added time constraints to notifications

--- backend/app/notifications.py
+++ backend/app/notifications.py
@@ -1,9 +1,63 @@
 from datetime import datetime
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Device, Notification, Plant, PlantData, User
 from app.router.utils import get_object_or_404
+
+
+# check if there is enough time between notifications for a plant
+async def check_recent_notification(plant: Plant, session: AsyncSession) -> bool:
+    time_now = datetime.now()
+    query = (
+        select(Notification)
+        .where(Notification.plant_id == plant.id)
+        .order_by(Notification.timestamp.desc())
+        .first()
+    )
+    notification = session.execute(query).scalars().first()
+
+    if notification:
+        time_delta = time_now - notification.timestamp
+        time_delta = time_delta.total_seconds() / 60
+    else:
+        return False
+
+    # wait a day if notification did not resolve
+    if (not notification.resolved) and (time_delta < 1440):
+        return False
+    # wait an hour if notification was resolved
+    elif (notification.resolved) and (time_delta < 60):
+        return False
+    else:
+        return True
+
+
+async def resolve_last_notification(plant: Plant, session: AsyncSession) -> None:
+    query = (
+        select(Notification)
+        .where(Notification.plant_id == plant.id)
+        .order_by(Notification.timestamp.desc())
+        .first()
+    )
+    notification = session.execute(query).scalars().first()
+    notification.resolved = True
+    await session.commit()
+
+
+# returns a list of grow property names from plant data that
+# are out of the desired range
+async def get_out_of_range_properties(plant_data: PlantData) -> list:
+    out_of_range = []
+
+    for reading in plant_data.sensor_readings:
+        if reading.value > reading.grow_property.max:
+            out_of_range.append(reading.grow_property.name)
+        elif reading.value < reading.grow_property.min:
+            out_of_range.append(reading.grow_property.name)
+
+    return out_of_range
 
 
 async def check_plant_properties(
@@ -13,25 +67,24 @@ async def check_plant_properties(
     # lookup device users
     # send notifications if needed to all users of device
 
-    out_of_range = []
+    should_send_notification = check_recent_notification(plant, session)
 
-    # check sensor readings in range
-    for reading in plant_data.sensor_readings:
-        if reading.value > reading.grow_property.max:
-            out_of_range.append(reading.grow_property.name)
-        elif reading.value < reading.grow_property.min:
-            out_of_range.append(reading.grow_property.name)
+    out_of_range = get_out_of_range_properties(plant_data)
 
     if len(out_of_range) > 0:
-        users = device.users
-        for user in users:
-            notification_data = {
-                "plant_id": plant.id,
-                "plant_name": plant.name,
-                "device_name": device.name,
-                "properties": out_of_range,
-            }
-            await create_user_notification(user, session, notification_data)
+        if should_send_notification:
+            users = device.users
+            for user in users:
+                notification_data = {
+                    "plant_id": plant.id,
+                    "plant_name": plant.name,
+                    "device_name": device.name,
+                    "properties": out_of_range,
+                }
+                await create_user_notification(user, session, notification_data)
+
+    else:
+        resolve_last_notification(plant, session)
 
 
 async def create_user_notification_by_id(user_id: int, session: AsyncSession) -> None:
