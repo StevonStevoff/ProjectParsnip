4451544f7b7196713cc2bc1cf9108b9302d19748
Steven Taylor <Steven@taylorcrew.us>
2023-04-15 00:20:09 +0100

    Added tests for getting plant data

--- backend/app/tests/test_plant_data.py
+++ backend/app/tests/test_plant_data.py
@@ -1,23 +1,82 @@
 from datetime import datetime
 
 import pytest
+from sqlalchemy import select
 
-from app.models import PlantData
-from app.tests.conftest import get_all_objects
-from app.tests.populate_tests import populate_db
+from app.models import PlantData, SensorReading
+from app.tests.conftest import get_all_objects, get_objects
+from app.tests.populate_tests import add_plant_data, add_sensor_readings, populate_db
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_get_plant_data_without_token(setup_db, client):
+    await populate_db(exclude=[PlantData, SensorReading])
+    response = await client.get("/plants/1/data")
+
+    assert response.status_code == 401
+    json_response = response.json()
+
+    assert json_response["detail"] == "Unauthorized"
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_get_plant_data_forbidden(setup_db, client, user_access_token):
+    headers = {"Authorization": f"Bearer {user_access_token}"}
+    response = await client.get("/plants/3/data", headers=headers)
+
+    assert response.status_code == 403
+    json_response = response.json()
+
+    assert json_response["detail"] == "Not owner or user of device with ID 2"
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_get_plant_data_no_data(setup_db, client, user_access_token):
+    headers = {"Authorization": f"Bearer {user_access_token}"}
+    response = await client.get("/plants/1/data", headers=headers)
+
+    assert response.status_code == 404
+    json_response = response.json()
+
+    plant_data_query = select(PlantData).where(PlantData.plant_id == 1)
+    plant_data = await get_objects(plant_data_query)
+
+    assert plant_data == []
+    assert json_response["detail"] == "No plant data found."
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_get_plant_data(setup_db, client, user_access_token):
+    await add_plant_data()
+    await add_sensor_readings()
+
+    headers = {"Authorization": f"Bearer {user_access_token}"}
+    response = await client.get("/plants/1/data", headers=headers)
+
+    assert response.status_code == 200
+    json_response = response.json()
+
+    plant_data_query = select(PlantData).where(PlantData.plant_id == 1)
+    plant_data = await get_objects(plant_data_query)
+    expected_timestamp = datetime(2023, 3, 6, 10, 12, 0, 123456)
+
+    assert len(plant_data) == len(json_response)
+    assert plant_data[0].timestamp == expected_timestamp
+    assert plant_data[0].timestamp.isoformat() == json_response[0]["timestamp"]
+    assert plant_data[0].plant_id == json_response[0]["plant_id"]
+    assert len(plant_data[0].sensor_readings) == len(
+        json_response[0]["sensor_readings"]
+    )
 
 
 @pytest.mark.skip(reason="Greenlet error, also update populate db")
 @pytest.mark.asyncio(scope="session")
 async def test_send_plant_data(setup_db, client, user_access_token):
-    await populate_db()
-
     headers = {"Authorization": f"Bearer {user_access_token}"}
     response = await client.post(
         "/plant_data/",
         headers=headers,
         json={
-            "plant_id": 1,
             "timestamp": str(datetime.now()),
             "device_id": 1,
             "sensor_readings": [
@@ -40,7 +99,6 @@ async def test_send_plant_data(setup_db, client, user_access_token):
 
     plant_data = await get_all_objects(PlantData)
     assert len(plant_data) == 1
-    assert plant_data[0].plant_id == 1
     assert len(plant_data[0].sensor_readings) == 2
     assert plant_data[0].sensor_readings[0].value == 55
     assert (
