7d3387a0e4ead35d38e3c09f6e6375830fd1c163
Omar Hashish <sc19orss@leeds.ac.uk>
2023-04-21 16:15:37 +0100

    Translation for paper dates

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
+import { enGB, registerTranslation } from 'react-native-paper-dates';
 import NavigationRoot from './src/navigation/NavigationRoot';
 import defaultTheme from './src/stylesheets/defaultTheme';
 
-import { enGB, registerTranslation } from 'react-native-paper-dates'
-registerTranslation('en-GB', enGB)
-
+registerTranslation('en-GB', enGB);
 
 function App() {
   const theme = defaultTheme();
