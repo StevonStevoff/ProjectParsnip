bf7ee587c470d5dc0d120a6e068ac29403197ac5
Steven Taylor <sc18sst@leeds.ac.uk>
2023-04-21 14:26:58 +0100

    Added device authentication logic, and applied to necessary routes

--- backend/app/authentication.py
+++ backend/app/authentication.py
@@ -1,22 +1,44 @@
-from typing import Annotated
+from typing import Annotated, Tuple
 
 import jwt
-from dotenv import dotenv_values
-from fastapi import Header, status
+import uuid
+
+from fastapi import Depends, Header, status
+from fastapi.security import APIKeyHeader
 from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import app.settings as secrets
+from app.database import get_async_session
 from app.models import User, Device
+from app.router.utils import get_object_or_404
+
+device_auth_header = APIKeyHeader(name="X-SECRET-DEVICE", scheme_name="device-header")
 
 
-async def create_device_token(device: Device) -> str:
-    payload = {"id": device.id}
-    return jwt.encode(payload, secrets.BACKEND_SECRET_KEY, "HS256")
+async def create_device_token(id: int) -> Tuple[str, str]:
+    token_uuid = str(uuid.uuid4().hex)
+    payload = {"device_id": id, "token_uuid": token_uuid}
+    device_token = jwt.encode(payload, secrets.BACKEND_SECRET_KEY, "HS256")
+    return device_token, token_uuid
 
 
-async def verify_device_token(device_token: Annotated[str, Header()]) -> bool:
-    pass
+async def verify_device_token(
+    device_token: Annotated[str, Header()], session: AsyncSession
+):
+    try:
+        payload = jwt.decode(
+            device_token, secrets.BACKEND_SECRET_KEY, algorithms=["HS256"]
+        )
+        device = await get_object_or_404(
+            payload.get("device_id"), Device, session, "The device does not exist."
+        )
+    except (HTTPException, jwt.DecodeError):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Invalid device token")
+    token_uuid = payload.get("token_uuid")
+    if token_uuid != device.token_uuid or device_token != device.auth_token:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Invalid device token")
+    return device
 
 
 async def verify_token(token: str, session: AsyncSession) -> User:
