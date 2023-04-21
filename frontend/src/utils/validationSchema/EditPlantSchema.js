5be7ac4f0f2121151b98a0d8327faf39f4ef0f74
Omar Hashish <sc19orss@leeds.ac.uk>
2023-04-21 11:14:56 +0100

    More Eslint error fixed (third time the charm)

--- frontend/src/utils/validationSchema/EditPlantSchema.js
+++ frontend/src/utils/validationSchema/EditPlantSchema.js
@@ -1,14 +1,14 @@
 import * as yup from 'yup';
 
 const EditPlantSchema = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    device_id: yup.number().required('Device ID is required'),
-    plant_profile_id: yup.number().required('Plant profile ID is required'),
-    plant_type_id: yup.number().required('Plant type ID is required'),
-    time_planted: yup.date().optional('Time planted is required'),
-    outdoor: yup.boolean().required('Outdoor is required'),
-    longitude: yup.number().optional('Longitude is optional').nullable(true),
-    latitude: yup.number().optional('Latitude is optional').nullable(true),
-  });
+  name: yup.string().required('Name is required'),
+  device_id: yup.number().required('Device ID is required'),
+  plant_profile_id: yup.number().required('Plant profile ID is required'),
+  plant_type_id: yup.number().required('Plant type ID is required'),
+  time_planted: yup.date().optional('Time planted is required'),
+  outdoor: yup.boolean().required('Outdoor is required'),
+  longitude: yup.number().optional('Longitude is optional').nullable(true),
+  latitude: yup.number().optional('Latitude is optional').nullable(true),
+});
 
-  export default EditPlantSchema;
+export default EditPlantSchema;
