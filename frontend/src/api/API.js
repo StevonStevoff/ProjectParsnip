863428badc47ee3590d44cae615291d3d9bb5d50
sc19sjjd <sc19sjjd@leeds.ac.uk>
2023-04-22 16:51:09 +0100

    Revert "Started setting up push notifications on frontend side"
    This reverts commit edd2df15613fb17f87e918db84ac05ef45d4e6ee.

--- frontend/src/api/API.js
+++ frontend/src/api/API.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-use-before-define */
-import qs from "qs";
-import api from "./config/axiosConfigs";
-import defineCancelApiObject from "./config/axiosUtils";
+import qs from 'qs';
+import api from './config/axiosConfigs';
+import defineCancelApiObject from './config/axiosUtils';
 
 const API = {
   async checkAPIConnection(cancel = false) {
     const response = await api.request({
-      url: "/",
-      method: "GET",
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      url: '/',
+      method: 'GET',
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
     return response;
   },
-  async registerUser({ email, password, name, username }, cancel = false) {
+  async registerUser({
+    email, password, name, username,
+  }, cancel = false) {
     const response = await api.request({
-      url: "/auth/register",
-      method: "POST",
+      url: '/auth/register',
+      method: 'POST',
       data: {
         email,
         password,
@@ -27,11 +27,9 @@ const API = {
         username,
       },
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
     return response;
@@ -39,19 +37,17 @@ const API = {
 
   async loginUser({ username, password }, cancel = false) {
     const response = await api.request({
-      url: "/auth/jwt/login",
-      method: "POST",
+      url: '/auth/jwt/login',
+      method: 'POST',
       data: qs
         .stringify({
           username,
           password,
         })
-        .replace(/%40/, "@"),
+        .replace(/%40/, '@'),
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        signal: cancel
-          ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-              .signal
+        'Content-Type': 'application/x-www-form-urlencoded',
+        signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
           : undefined,
       },
     });
@@ -64,11 +60,9 @@ const API = {
   },
   async logout(cancel = false) {
     const response = await api.request({
-      url: "/auth/jwt/logout",
-      method: "POST",
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      url: '/auth/jwt/logout',
+      method: 'POST',
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
     return response;
@@ -76,11 +70,9 @@ const API = {
 
   async getAuthenticatedUser(cancel = false) {
     const response = await api.request({
-      url: "/authenticated-route",
-      method: "GET",
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      url: '/authenticated-route',
+      method: 'GET',
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
@@ -89,11 +81,9 @@ const API = {
 
   async getUserInfo(cancel = false) {
     const response = await api.request({
-      url: "/users/me",
-      method: "GET",
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      url: '/users/me',
+      method: 'GET',
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
@@ -102,11 +92,9 @@ const API = {
 
   async getAllUsers(cancel = false) {
     const response = await api.request({
-      url: "/users/",
-      method: "GET",
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      url: '/users/',
+      method: 'GET',
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
     return response;
@@ -114,19 +102,17 @@ const API = {
 
   async updateUserInfo({ name, email, username }, cancel = false) {
     const response = await api.request({
-      url: "/users/me",
-      method: "PATCH",
+      url: '/users/me',
+      method: 'PATCH',
       data: {
         name,
         email,
         username,
       },
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
@@ -136,11 +122,9 @@ const API = {
   async getUsersDevices(cancel = false) {
     try {
       const response = await api.request({
-        url: "/devices/me",
-        method: "GET",
-        signal: cancel
-          ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-              .signal
+        url: '/devices/me',
+        method: 'GET',
+        signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
           : undefined,
       });
 
@@ -154,11 +138,9 @@ const API = {
   },
   async getCurrentUsersPlants(cancel = false) {
     const response = await api.request({
-      url: "/plants/me",
-      method: "GET",
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      url: '/plants/me',
+      method: 'GET',
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
@@ -169,14 +151,12 @@ const API = {
     const response = await api.request({
       // eslint-disable-next-line no-template-curly-in-string
       url: `devices/${data.id}`,
-      method: "PATCH",
+      method: 'PATCH',
       data,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
@@ -184,14 +164,12 @@ const API = {
   },
   async getSensors(cancel = false) {
     const response = await api.request({
-      url: "/sensors/",
-      method: "GET",
+      url: '/sensors/',
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
@@ -200,34 +178,15 @@ const API = {
   async deleteDevice({ device }, cancel = false) {
     const response = await api.request({
       url: `/devices/${device.id}`,
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
+      signal: cancel ? cancelApiObject[this.getPaginated.name].handleRequestCancellation().signal
         : undefined,
     });
 
     return response;
-  },
-
-  async registerPushToken({ pushToken }, cancel = false) {
-    const response = await api.request({
-      url: `/users/me/setPushToken`,
-      method: "POST",
-      data: {
-        pushToken,
-      },
-      headers: {
-        "Content-type": "application/json",
-      },
-      signal: cancel
-        ? cancelApiObject[this.getPaginated.name].handleRequestCancellation()
-            .signal
-        : undefined,
-    });
   },
 };
 
