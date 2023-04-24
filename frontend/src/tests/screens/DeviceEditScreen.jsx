18085a781f9795277f6b9b961850658813d781ae
Aimee <aimeeboyle6@gmail.com>
2023-04-24 15:50:48 +0100

    Update the edit screen test

--- frontend/src/tests/screens/DeviceEditScreen.jsx
+++ frontend/src/tests/screens/DeviceEditScreen.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render, waitFor, cleanup } from '@testing-library/react-native';
+import {
+  render, waitFor, cleanup,
+} from '@testing-library/react-native';
 import { NativeBaseProvider } from 'native-base';
-import DeviceDetailsScreen from '../../screens/DevicesScreens/DevicesDetailsScreen';
+import DeviceEditScreen from '../../screens/DevicesScreens/DevicesDetailsScreen';
 import DeviceUtils from '../../api/utils/DeviceUtils';
 import defaultTheme from '../../stylesheets/defaultTheme';
 
@@ -27,36 +29,24 @@ const mockDevices = [
   },
 ];
 
-describe('DevicesDetailsScreen', () => {
+describe('DevicesEditScreen', () => {
   afterEach(() => {
     cleanup();
   });
   const theme = defaultTheme();
 
-  test('displays error message when device details retrieval fails', async () => {
-    DeviceUtils.getDeviceDetails.mockRejectedValueOnce(new Error('Failed to fetch device details'));
-    const { getByText } = render(
-      <NativeBaseProvider theme={theme}>
-        <DeviceDetailsScreen route={{ params: { deviceId: '1' } }} />
-      </NativeBaseProvider>,
-    );
-
-    await waitFor(() => {
-      expect(getByText('Failed to fetch device details')).toBeDefined();
-    });
-  });
-
   test('renders device details correctly', async () => {
     DeviceUtils.getDeviceDetails.mockResolvedValueOnce(mockDevices[0]);
     const { findByText } = render(
       <NativeBaseProvider theme={theme}>
-        <DeviceDetailsScreen route={{ params: { deviceId: '1' } }} />
+        <DeviceEditScreen route={{ params: { deviceId: '1' } }} />
       </NativeBaseProvider>,
     );
-
     await waitFor(() => {
       expect(findByText('Device 1')).toBeDefined();
       expect(findByText('Model 1')).toBeDefined();
     });
   });
+
+  // Add more tests as needed
 });
